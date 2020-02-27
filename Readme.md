91afad9bd840f34c45fc123274580cf777af5f0a
ArindamSharma <arindamsharma1998@gmail.com>
2020-02-27 14:11:54 +0530

    Update config-1.sh

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 
 # Run config-1.sh only once
+/Config-1.sh  File will 
 # Run config-2.sh when ever there is a new traning data set
--- config-1.sh
+++ config-1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cd ../
+var=$(ls | grep darknet)
+len=${#var}
+if [ $len == '7' ] ; then
+	echo "---Make sure Internet is there---"
+	rm darknet -R
+else
+	echo "---Make sure Internet is there---"
+fi
 git clone https://github.com/pjreddie/darknet
 cd darknet/src
 sed -i "426s/str.*/strcpy(copy, s);/g" utils.c
